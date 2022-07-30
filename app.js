a2d30ed6c90d2c62a57c65045944413b2e6096e0
Osvaldo <ojimenez@pacific.edu>
2022-07-29 23:07:20 -0700

    did we finally fix 11.11 to show 11.12 too?

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const app = express()
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
-app.use(express.static('dist'))
-
 app.get('/version', (req, res) => {
   res.send('1')
 })
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
